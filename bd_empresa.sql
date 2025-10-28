0c233fdad9d0f4b8c91259ea2095413523061149
anyelors <anyelors@gmail.com>
2025-10-28 13:00:23 +0100

    BD Empresa

---
+++ bd_empresa.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA `empresa`;
+
+CREATE TABLE `empresa`.`departamentos` (
+  `iddepartamento` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`iddepartamento`));
+
+CREATE TABLE `empresa`.`empleados` (
+  `idempleado` INT NOT NULL AUTO_INCREMENT,
+  `dni` VARCHAR(45) NOT NULL UNIQUE,
+  `nombre` VARCHAR(45) NOT NULL,
+  `sueldo` DECIMAL(8,2),
+  `iddepartamento` INT NOT NULL,
+  PRIMARY KEY (`idempleado`),
+  INDEX `iddepartamento_idx` (`iddepartamento` ASC) VISIBLE,
+  CONSTRAINT `iddepartamento`
+    FOREIGN KEY (`iddepartamento`)
+    REFERENCES `empresa`.`departamentos` (`iddepartamento`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
