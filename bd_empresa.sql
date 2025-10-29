422e6322a3b0ea44a9b8ac4aaf45f3ef4db83fcf
anyelors <anyelors@gmail.com>
2025-10-29 10:15:36 +0100

    BD Empresa Ajustes

--- bd_empresa.sql
+++ bd_empresa.sql
@@ -2,14 +2,14 @@ CREATE SCHEMA `empresa`;
 
 CREATE TABLE `empresa`.`departamentos` (
   `iddepartamento` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL UNIQUE,
   PRIMARY KEY (`iddepartamento`));
 
 CREATE TABLE `empresa`.`empleados` (
   `idempleado` INT NOT NULL AUTO_INCREMENT,
   `dni` VARCHAR(45) NOT NULL UNIQUE,
   `nombre` VARCHAR(45) NOT NULL,
-  `sueldo` DECIMAL(8,2),
+  `sueldo` DECIMAL(8,2) NOT NULL,
   `iddepartamento` INT NOT NULL,
   PRIMARY KEY (`idempleado`),
   INDEX `iddepartamento_idx` (`iddepartamento` ASC) VISIBLE,
